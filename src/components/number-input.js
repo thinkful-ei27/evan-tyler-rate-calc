7221b322765a030c75a15da8ac969871deed5912
Evan Guirino <etg624@gmail.com>
2019-01-23 10:53:00 -0800

    first commit

--- src/components/number-input.js
+++ src/components/number-input.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 export default function NumberInput(props) {
-    return (
-        <div className="form-group">
-            <label htmlFor={props.id}>{props.label}</label>
-            <input type="number" id={props.id} min={props.min} max={props.max} />
-        </div>
-    );
+  return (
+    <div className="form-group">
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        name={props.name}
+        type="number"
+        id={props.id}
+        min={props.min}
+        max={props.max}
+        onChange={e => props.handleChange(e)}
+      />
+    </div>
+  );
 }
-
-
--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -4,20 +4,45 @@ import NumberInput from './number-input';
 import Output from './output';
 
 export default class RateCalculator extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dayRate: 0,
+      hours: 0
+    };
+  }
+
+  handleChange(e) {
+    let name = e.target.name;
+    this.setState({
+      [name]: Number(e.target.value)
+    });
+  }
+
+  render() {
+    const rate = this.state.dayRate / this.state.hours;
+    if (rate === NaN || rate === Infinity) {
     }
 
-    render() {
-        const rate = 10;
-        return (
-            <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
-                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
-/>
-            </form>
-        );
-    }
+    return (
+      <form>
+        <NumberInput
+          name="dayRate"
+          handleChange={e => this.handleChange(e)}
+          label="Day rate"
+          min={0}
+          max={5000}
+        />
+        <NumberInput
+          name="hours"
+          handleChange={e => this.handleChange(e)}
+          label="Hours"
+          min={1}
+          max={12}
+        />
+        <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)} />
+      </form>
+    );
+  }
 }
-
